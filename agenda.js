c0d0045bd4e7f953e2dfd388c4dc232ebc238a06
test <test>
2022-04-05 01:38:02 +0000

    test

---
+++ agenda.js
@@ -0,0 +1,18 @@
+module.exports = class Agenda extends Array
+{
+
+	add(node)
+	{
+		this.push(node)
+	}
+
+	getNode()
+	{
+		return this.pop()
+	}
+
+	notEmpty()
+	{
+		return this.length !== 0
+	}
+}
